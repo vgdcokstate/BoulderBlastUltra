a4b44804a6ccb6097a94e2cb6435c4fe7f4e5e46
Brayden Dyke <brayden.dyke@gmail.com>
2019-11-07 15:12:32 -0600

    Adding Dynamic Lava Material

---
+++ Assets/Scripts/LavaScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaScript : MonoBehaviour
+{
+    // Scroll main texture based on time
+
+    private float scrollSpeed = 0.1f;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, offset * 0.5f));
+    }
+}
